5d285bdcc086da9b01fa3a4b39ced815614ea9e7
Yonathan Allouch <yonathan.allouch@post.idc.ac.il>
2024-01-16 18:59:12 +0200

    First implemtation that create the file

--- ShowSuggesterAI.py
+++ ShowSuggesterAI.py
@@ -2,7 +2,6 @@ import csv
 import pickle
 import os
 import time
-##from openai import OpenAI
 import openai
 import logging
 from dotenv import load_dotenv
@@ -27,24 +26,33 @@ def load_tv_shows(csv_file):
     return shows
        
 
-def get_embeddings(show_descriptions, api_key):
+def get_embeddings(show_descriptions, api_key, batch_size=200):
     openai.api_key = api_key
 
     embeddings = {}
-    request_count = 0
+    descriptions_batch = []
+    titles_batch = []
 
     for title, description in show_descriptions.items():
+        descriptions_batch.append(description)
+        titles_batch.append(title)
+        # Once batch size is reached, or end of descriptions is reached, make the API call
+    
+    if len(descriptions_batch) == batch_size or title == list(show_descriptions.keys())[-1]:
         try:
             response = openai.Embedding.create(
                 model="text-embedding-ada-002",
-                input=[description] 
+                input=descriptions_batch
             )
-            embeddings[title] = response['data'][0]['embedding']
-            request_count += 1
+            for i, embedding in enumerate(response['data']):
+                embeddings[titles_batch[i]] = embedding['embedding']
+                
+                # Reset the batches
+            descriptions_batch = []
+            titles_batch = []
 
-            # Add a delay every 3 requests
-            if request_count % 3 == 0:
-                time.sleep(20)
+                # Wait 60 seconds to respect the rate limit
+                #time.sleep(60)
 
         except Exception as e:
             logging.error(f"Error fetching embeddings for {title}: {e}")
@@ -100,10 +108,7 @@ def main():
             embeddings = get_embeddings(shows, api_key)
             save_embeddings(embeddings, pickle_file_path)
     except Exception as e:
-        logging.error(f"An error occurred: {e}")
-
-        # Your further processing with embeddings can go here
-        # ...
+        logging.error(f"An error occurred: {e}")     
 
 if __name__ == "__main__":
     main()
